9290316dcbe7dd47a3af1847653384d5a03cffb5
Dev Shah <itsdev@MacBook-Pro.local>
2014-11-13 13:57:54 -0500

    Added simple server on port 9615 and create table users

---
+++ server.js
@@ -0,0 +1,34 @@
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('Users', createTable);
+
+insertRows()
+var data2;
+values = db.all("select * from users;", function(err, data) {
+        console.log(data); 
+        data2 = data;
+    });
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(data2.toString());
+  console.log(req)
+}).listen(9615);
+
+
+function createTable() {
+    console.log("createTable users");
+    db.run("CREATE TABLE IF NOT EXISTS users (first_name char(50), last_name char(50), email char(100), team char(100));");
+}
+
+function insertRows(first_name, last_name, email, team) {
+    console.log("insertRows users");
+    var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?)");
+
+    stmt.run(first_name, last_name, email, team);
+    
+
+}
