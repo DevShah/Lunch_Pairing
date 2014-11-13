e35f2ffba28852c5b09d082964ef81ec16377bba
DevShah <bullet02@gmail.com>
2014-11-13 17:16:38 -0500

    Added show all users

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('Users', createTable);
+var db = new sqlite3.Database('Users', createTableUsers);
 
-values = db.all("select * from users;", function(err, data) {
-        console.log(data); 
-        data2 = data;
-    });
+// values = db.all("select * from users;", function(err, data) {
+//         console.log(data); 
+//         data2 = data;
+//     });
 
 var http = require('http');
 var fs = require('fs');
-
+var datetime = new Date();
+console.log(datetime);
 http.createServer(function (req, res) {
 
     if (req.method == 'POST') {
         var body = '';
         req.on('data', function (data) {
             var json = JSON.parse(data.toString())
-            if(json.method == 'insert_User'){
+            if(json.method == 'insert_user'){
                 insert_User(json.params[0],json.params[1],json.params[2],json.params[3])
+                console.log(db.run("select * from users;"))
             }
-            console.log(json);
+            else if(json.method == 'deactivate_user'){
+                deactivate_User(json.params[0],json.params[1],json.params[2],json.params[3])
+            }
+            else if(json.method == 'show_all_users'){
+                        db.all("select * from users;",function show_all_users(err, all){
+                            res.writeHead(200, {'Content-Type': 'text/plain'});
+                            res.end(JSON.stringify(all))
+                        });
+            }
         });
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Post Request");
+        
+
     }
     else{
       res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -33,27 +43,57 @@ http.createServer(function (req, res) {
  the_interval = 24 * 7 * 60 * 60 * 1000;
 setInterval(function() {
   console.log("One week update lunches");
-  // do your stuff here
+  calculate_new_pairing()
 }, the_interval);
 
-function createTable() {
+function createTableUsers() {
     console.log("createTable users");
-    db.run("CREATE TABLE IF NOT EXISTS users (first_name char(50), last_name char(50), email char(100), team char(100));");
+    db.run("CREATE TABLE IF NOT EXISTS users (first_name char(50), last_name char(50), email char(100), team char(100), active INTEGER);");
 }
 
-function insert_User(first_name, last_name, email, team) {
-    console.log("insertRows users");
-    var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?)");
-    stmt.run(first_name, last_name, email, team);
+function createTableEvents() {
+    console.log("createTable events");
+    db.run("CREATE TABLE IF NOT EXISTS events (email1 char(100), email2 char(100), event_type char(200), photo char(200));");
 }
 
-function delete_User(email) {
+
+
+/*
+    Insert user into the database
+
+*/
+function insert_user(first_name, last_name, email, team) {
     console.log("insertRows users");
-    var stmt = db.prepare("DELETE from Users where email = (?);");
+    var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?,?)");
+    stmt.run(first_name, last_name, email, team, 1);
+}
+
+
+
+
+/*
+    Deactivate user
+*/
+function deactivate_user(email) {
+    console.log("insertRows users");
+    var stmt = db.prepare("UPDATE users SET active=0 where email = (?);");
     stmt.run(email);
 }
 
 function calculate_new_pairing(){
+
+}
+
+function show_current_week_pairing(){
+    offset = 0
+    if(Date().indexOf("Mon") > -1){
+        offset = 0
+    }
+    else if(Date().indexOf("Mon") > -1)
+    {
+        offset = 1
+    }
+    db.prepare("SELECT * from events where date = ")
 
 }
 
