2f04fc03ded8466aae786c3e63132e601c396433
DevShah <bullet02@gmail.com>
2014-11-14 09:40:01 -0500

    Added show current weeks pairing

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('Users', createTableUsers);
-
+var db_event = new sqlite3.Database('Events', createTableEvents);
+var moment = require('moment');
 // values = db.all("select * from users;", function(err, data) {
 //         console.log(data); 
 //         data2 = data;
@@ -22,13 +23,37 @@ http.createServer(function (req, res) {
                 console.log(db.run("select * from users;"))
             }
             else if(json.method == 'deactivate_user'){
-                deactivate_User(json.params[0],json.params[1],json.params[2],json.params[3])
+                deactivate_user(json.params[2])
+            }
+            else if(json.method == 'activate_user'){
+                activate_user(json.params[2])
             }
             else if(json.method == 'show_all_users'){
-                        db.all("select * from users;",function show_all_users(err, all){
-                            res.writeHead(200, {'Content-Type': 'text/plain'});
-                            res.end(JSON.stringify(all))
-                        });
+                db.all("select * from users;",function show_all_users(err, all){
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.write(JSON.stringify(all))
+                    res.end()
+                });
+            }
+            else if(json.method == 'show_current_week_pairing'){
+                date = moment().day(1).format('YYYYMMDD')
+                db_event.all("SELECT * from events where date = (?)", date,
+                    function show_current_week_pairing(err, all){
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.write(JSON.stringify(all))
+                        res.end()
+                });
+            }
+            else if(json.method == 'calculate_new_pairing') {
+                calculate_new_pairing()
+            }
+            else if(json.method == 'show_all_weeks_pairings'){
+                db_event.all("SELECT * from events",
+                    function show_current_week_pairing(err, all){
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.write(JSON.stringify(all))
+                        res.end()
+                });
             }
         });
         
@@ -53,7 +78,7 @@ function createTableUsers() {
 
 function createTableEvents() {
     console.log("createTable events");
-    db.run("CREATE TABLE IF NOT EXISTS events (email1 char(100), email2 char(100), event_type char(200), photo char(200));");
+    db_event.run("CREATE TABLE IF NOT EXISTS events (date char(100), email1 char(100), email2 char(100), event_type char(200), photo char(200));");
 }
 
 
@@ -69,8 +94,6 @@ function insert_user(first_name, last_name, email, team) {
 }
 
 
-
-
 /*
     Deactivate user
 */
@@ -80,20 +103,28 @@ function deactivate_user(email) {
     stmt.run(email);
 }
 
+/*
+    Activate user
+*/
+function activate_user(email) {
+    console.log("insertRows users");
+    var stmt = db.prepare("UPDATE users SET active=1 where email = (?);");
+    stmt.run(email);
+}
+
 function calculate_new_pairing(){
 
-}
+    var date = moment().format('YYYYMMDD')
+    db_event.each("SELECT email from users",
+            function blah(err, all){
+                var email = all.email
+                db.run("SELECT date from events where email1=(y) OR email2=(y);", function(err,data){
+                    if()
+                });
 
-function show_current_week_pairing(){
-    offset = 0
-    if(Date().indexOf("Mon") > -1){
-        offset = 0
-    }
-    else if(Date().indexOf("Mon") > -1)
-    {
-        offset = 1
-    }
-    db.prepare("SELECT * from events where date = ")
+            });
 
 }
+
+
 
